22b3e12981e7b196109bb75dc0b09a362a4b26f2
anikethanchalia <aniketh.anchalia@smartbear.com>
2024-08-21 12:05:39 +0530

    First commit of userAuthorisation

---
+++ userAuthorisation/src/main/java/com/example/userAuthorisation/UserAuthorisationApplication.java
@@ -0,0 +1,13 @@
+package com.example.userAuthorisation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthorisationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAuthorisationApplication.class, args);
+	}
+
+}
---
+++ userAuthorisation/src/test/java/com/example/userAuthorisation/UserAuthorisationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userAuthorisation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthorisationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
