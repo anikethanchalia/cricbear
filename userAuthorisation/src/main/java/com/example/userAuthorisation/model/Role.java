eeeb78f89055768a862d581aa37939b908ff9275
anikethanchalia <aniketh.anchalia@smartbear.com>
2024-08-21 16:26:51 +0530

    User model class

---
+++ userAuthorisation/src/main/java/com/example/userAuthorisation/model/Role.java
@@ -0,0 +1,5 @@
+package com.example.userAuthorisation.model;
+
+public enum Role {
+    ADMIN, COACH, PLAYER
+}
---
+++ userAuthorisation/src/main/java/com/example/userAuthorisation/model/User.java
@@ -0,0 +1,75 @@
+package com.example.userAuthorisation.model;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Column(name = "uid")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int uid;
+
+    @Column(name = "username",unique = true)
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "role")
+    private Role role;
+
+    public User() {
+
+    }
+    public User(String username, String password, String email, Role role) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
